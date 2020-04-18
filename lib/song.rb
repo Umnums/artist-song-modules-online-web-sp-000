82a60dcad68486211daed44a7ee7457835c66aee
Joseph Kao <josephtkao@gmail.com>
2020-04-18 03:08:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,9 +32,9 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
 
 this = Song.new
